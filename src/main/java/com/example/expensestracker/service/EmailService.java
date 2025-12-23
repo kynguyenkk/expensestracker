78e7e5cdb28ab64251bc480a537b18f9504537e7
kynguyen <kybongdem@gmail.com>
2025-12-23 21:28:23 +0700

    update security

--- src/main/java/com/example/expensestracker/service/EmailService.java
+++ src/main/java/com/example/expensestracker/service/EmailService.java
@@ -3,9 +3,11 @@ package com.example.expensestracker.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@Async
 public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
